4d6df815aac7e49b72ad268a5b84ab96b7af7e02
KyznecSPM <eme.di@ya.ru>
2019-02-17 00:00:25 +0300

    refactoring, add functional: delete, markup important, done.

---
+++ src/components/app/app.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list/todo-list";
+import ItemStatusFilter from "../item-status-filter";
+
+import "./app.css";
+
+class App extends Component {
+  state = {
+    todoData: [
+      { label: "Drink Coffee", important: false, id: 1 },
+      { label: "Learn React", important: true, id: 2 },
+      { label: "Have a lunch", important: false, id: 3 }
+    ]
+  };
+
+  deleteItem = id => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex(el => el.id === id);
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+      </div>
+    );
+  }
+}
+
+export default App;
